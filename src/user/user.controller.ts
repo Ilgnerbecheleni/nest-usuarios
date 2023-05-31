de733df96b0317f42348bb75a91adf25b770b3b1
ilgner becheleni <ilgner.gui@gmail.com>
2023-05-30 22:10:35 -0300

    auth

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,8 +1,9 @@
 /* eslint-disable prettier/prettier */
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards } from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { AuthGuard } from 'src/auth/guard/auth.guard';
 
 @Controller('user')
 export class UserController {
@@ -13,7 +14,9 @@ export class UserController {
     return this.userService.create(createUserDto);
   }
 
+  @UseGuards(AuthGuard)
   @Get()
+ 
   findAll() {
     return this.userService.findAll();
   }
